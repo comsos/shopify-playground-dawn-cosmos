ac785202c85d15522540eb2f238905ffe1c50198
cosmos <paulchristian946@gmail.com>
2023-08-15 13:13:58 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    '/templates/customer/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
